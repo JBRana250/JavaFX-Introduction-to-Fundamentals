3c14b8dce49b5c43c7c196c86a545390c570e81b
JB Rana <JBRana2500@gmail.com>
2026-07-21 20:27:53 +1200

    Play music with MediaPlayer

--- src/main/java/nz/ac/auckland/se206/controllers/MusicPlayerController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MusicPlayerController.java
@@ -1,13 +1,20 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.net.URISyntaxException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class MusicPlayerController {
+
+    MediaPlayer player;
     
     @FXML
     private Button firstSongButton;
@@ -16,14 +23,30 @@ public class MusicPlayerController {
     @FXML
     private Button switchButton;
 
+    private final String firstSongPath = "/sounds/looking-forward-131923.mp3";
+    private final String secondSongPath = "/sounds/risk-136788.mp3";
+
     @FXML
     private void playFirstSong() {
-
+        playSong(firstSongPath);
     }
 
     @FXML
     private void playSecondSong() {
+        playSong(secondSongPath);
+    }
 
+    private void playSong(String songPath) {
+        try {
+            Media sound = new Media(App.class.getResource(songPath).toURI().toString());
+            if (player != null) {
+                player.stop();
+            }
+            player = new MediaPlayer(sound);
+            player.play();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
